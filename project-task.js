5a1404d121f0de03ee3ad64ccd5084c22807231b
chrissy <chrissyrodgers20@gmail.com>
2025-05-15 18:35:15 -0500

    Completed all TODOs during mob programming

--- project-task.js
+++ project-task.js
@@ -45,18 +45,28 @@ Step 4: Test Your Solution
 function processFile(fileName, fileData) {
   try {
     // TODO: Add input validation here
-    
+      if (!fileName) {
+        throw new Error("Missing filename."); 
+      }if (typeof fileData !== "string") {
+        throw new Error("File Data is not a string");
+      } if(!fileData) {
+        throw new Error("File data is empty");
+
+       } 
     // TODO: Implement simulated file processing here
     console.log(`Processing file: ${fileName}`);
     console.log(`File content: ${fileData}`);
-    
+        
+      }
     // TODO: Add simulated file operations (reading/writing)
-    
-  } catch (err) {
+    catch (err) {
     // TODO: Implement error handling
-    console.error(err);
+    console.error(err.message);
+  } finally {
+    // TODO: Implement a finally block to close resources
+    console.log("Closing resources.");
   }
-  // TODO: Implement a finally block to close resources
+  
 }
 
 // ============================================
